bbafa5833a1bcf6a2d4307f696cd9e9fda9cf97d
Emmanuel Cañate <emmanuelcanate@gmail.com>
2024-06-15 13:57:12 -0400

    fix: some corrections

--- src/modules/diagnostic/dtos/diagnostic.dto.ts
+++ src/modules/diagnostic/dtos/diagnostic.dto.ts
@@ -1,6 +1,5 @@
 import { AbstractDto } from "../../../common/dto/abstract.dto";
 import { DateField, StringField, ClassField } from "../../../decorators";
-import { AppointmentDto } from "../../../modules/appointment/dtos/appointment.dto";
 import { CancerStageDto } from "../../../modules/cancer/dtos/cancer-stage.dto";
 import { CancerTypeDto } from "../../../modules/cancer/dtos/cancer-type.dto";
 import { DiagnosticEntity } from "../entities/diagnostic.entity";
@@ -16,9 +15,6 @@ export class DiagnosticDto extends AbstractDto {
   @StringField()
   notes!: string;
 
-  @ClassField(() => AppointmentDto)
-  appointment!: AppointmentDto;
-
   @ClassField(() => CancerTypeDto)
   cancerType!: CancerTypeDto;
 
@@ -30,7 +26,6 @@ export class DiagnosticDto extends AbstractDto {
     this.closedAt = diagnostic.closedAt;
     this.date = diagnostic.date;
     this.notes = diagnostic.notes;
-    this.appointment = diagnostic.appointment.toDto();
     this.cancerType = diagnostic.cancerType.toDto();
     this.cancerStage = diagnostic.cancerStage.toDto();
   }
--- src/modules/supplies/supplies.module.ts
+++ src/modules/supplies/supplies.module.ts
@@ -7,9 +7,10 @@ import { SuppliesEntity } from './entities/supplies.entity';
 import { SuppliesController } from './supplies.controller';
 import { MeasurementUnitController } from './measurement-unit.controller';
 import { SuppliesService } from './supplies.service';
+import { MeasurementUnitEntity } from './entities/measurement-unit.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SuppliesEntity]), PersonModule, OncologyCenterModule],
+  imports: [TypeOrmModule.forFeature([SuppliesEntity, MeasurementUnitEntity]), PersonModule, OncologyCenterModule],
   controllers: [SuppliesController, MeasurementUnitController],
   exports: [],
   providers: [SuppliesService],
